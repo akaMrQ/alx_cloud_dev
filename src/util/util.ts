f79704e5cdc6b24687d8b3026cd2113e80f4d65e
jacquesk <jkammeyer@sataxi.co.za>
2023-01-08 12:28:34 +0200

    [project2] : fix node fetch, downgraded.

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import Jimp = require("jimp");
+import fetch from 'node-fetch';
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
